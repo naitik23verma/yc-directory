921a277c16e806b585fb09af2bffc4303a230144
Naitik Verma <naitikv2311@gmail.com>
2025-06-30 22:41:11 +0530

    ok

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = async () => {
         <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
             <nav className='flex justify-between items-center'>
                 <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={144} height={30} ></Image>
+                    <Image src="/logo.svg" alt="logo" width={144} height={30} ></Image>
                 </Link>
 
                 <div className='flex items-center gap-5 text-black' >
