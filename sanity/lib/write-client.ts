c112c50fe8a47837a2b9b82deebecc6a1fcc7970
Naitik Verma <naitikv2311@gmail.com>
2025-07-01 01:14:42 +0530

    ok

--- sanity/lib/write-client.ts
+++ sanity/lib/write-client.ts
@@ -1,16 +1,15 @@
-import "server-only"
+import "server-only";
+import { createClient } from "next-sanity";
+import { apiVersion, dataset, projectId, token } from "../env";
 
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId,token } from '../env'
+if (process.env.NODE_ENV === "production" && !token) {
+  throw new Error("❌ SANITY_API_TOKEN is missing in production.");
+}
 
 export const writeClient = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: false,
-  token // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
-if (!writeClient.config().token){
-    throw new Error("Write token not found");
-}
+  token, // Only set if available
+});
