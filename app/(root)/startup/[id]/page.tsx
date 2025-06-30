7ed8b8b5ed84858f71d0fd5e97dbe5969768f72e
Naitik Verma <naitikv2311@gmail.com>
2025-06-30 20:43:00 +0530

    ok

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -13,6 +13,7 @@ export const experimental_ppr =true;
 const Page = async ({params}:{params:Promise<{id:string}>}) => {
     const id=(await params).id;
     const post=await client.fetch(STARTUP_BY_ID_QUERY,{id})
+    
     if (!post) return notFound();
     const parsedContent=md.render(post?.pitch || '')
   return (
